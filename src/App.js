9781738d80daafd541434a5252b2dad8ffee8059
DanyhMA <danielamaraujo1993@gmail.com>
2022-10-20 16:30:52 -0300

    atividade 04

--- src/App.js
+++ src/App.js
@@ -1,22 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
+
+function Usuario(props){
+  return <h1>{props.texto}</h1>;
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div>
+      <header class='App-header'>
+        <Usuario texto='nome' />
+        <Usuario texto='comentário' />
       </header>
     </div>
   );
